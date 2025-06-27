7b761fe8d6c52a06d94cfd60199374aace947eee
Sam-Bendelow <sambendelow5@gmail.com>
2025-06-27 02:35:08 +0100

    Delete help_desk_app directory

--- help_desk_app/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or ('erfhviueviueb')
-    BASEDIR = os.path.abspath(os.path.dirname(__file__))
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASEDIR, 'instance', 'service_desk.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
